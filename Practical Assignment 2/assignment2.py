f5f27c6c0c91ea4f83b9db135a62ed48095cd2b9
ScaleRunner <dennis-verheijden@live.nl>
2017-09-22 13:31:41 +0200

    PA2.2 model saving and retrieval

--- Practical Assignment 2/assignment2.py
+++ Practical Assignment 2/assignment2.py
@@ -4,6 +4,7 @@ import Networks
 import chainer
 from chainer.links import Classifier
 import matplotlib.pyplot as plt
+import pickle
 
 # Import whole dataset
 train, test = chainer.datasets.get_mnist(ndim=3)
@@ -16,17 +17,35 @@ batch_size = 32
 train_iter = utils.RandomIterator(train, batch_size)
 test_iter = utils.RandomIterator(test, batch_size)
 
-# Standard classifier that uses softmax_cross_entropy as loss function
-net = Networks.Convolutional()
-model = Classifier(net)
-optimizer = optimizers.SGD()
-optimizer.setup(model)
 
-train_loss_list = []
-test_loss_list = []
+def get_model(model_name):
+    try:
+        pickle_in = open("{}_model.pickle".format(model_name), 'rb')
+        model = pickle.load(pickle_in)
+
+        pickle_in = open("{}_optimizer.pickle".format(model_name), 'rb')
+        optimizer = pickle.load(pickle_in)
+
+        pickle_in = open("{}_results.pickle".format(model_name), 'rb')
+        results = pickle.load(pickle_in)
+    except FileNotFoundError:
+        # Standard classifier that uses softmax_cross_entropy as loss function
+        model = Classifier(Networks.Convolutional())
+        optimizer = optimizers.SGD()
+        optimizer.setup(model)
+        print("Model not found! Starting training ...")
+        results = train_network(model, optimizer)
+        with open('convnet_model.pickle', 'wb') as f:
+            pickle.dump(model, f)
+        with open('convnet_optimizer.pickle', 'wb') as f:
+            pickle.dump(optimizer, f)
+        with open('convnet_results.pickle', 'wb') as f:
+            pickle.dump(results, f)
+
+    return model, optimizer, results
 
 
-def feed_data(random_iter, update):
+def feed_data(model, optimizer, random_iter, update):
     """
     Feeds the network data
     :param random_iter: Iterator that holds the data
@@ -48,14 +67,18 @@ def feed_data(random_iter, update):
     return total_loss / random_iter.idx, total_accuracy / random_iter.idx
 
 
-def run():
+def train_network(model, optimizer):
     """
-    Trains the MLP network for n_epochs.
+    Trains the network for n_epochs.
     One epoch contains of a training phase and testing phase. Afterwards, the results are printed to the screen
     """
+
+    train_loss_list = []
+    test_loss_list = []
+
     for epoch in range(n_epochs):
-        train_loss, train_accuracy = feed_data(train_iter, True)
-        test_loss, test_accuracy = feed_data(test_iter, False)
+        train_loss, train_accuracy = feed_data(model, optimizer, train_iter, True)
+        test_loss, test_accuracy = feed_data(model, optimizer, test_iter, False)
 
         train_loss_list.append(train_loss)
         test_loss_list.append(test_loss)
@@ -64,11 +87,28 @@ def run():
               'Testing: accuracy: {} \t loss: {}'.format(epoch + 1,
                                                          train_accuracy, train_loss,
                                                          test_accuracy, test_loss))
+    return [train_loss_list, test_loss_list]
 
 
-run()
-plt.plot(test_loss_list, label='Test Loss')
-plt.plot(train_loss_list, label='Train Loss')
+def test_network(model, optimizer):
+    """
+    Tests the conv network
+    """
+
+    train_loss, train_accuracy = feed_data(model, optimizer, train_iter, False)
+    test_loss, test_accuracy = feed_data(model, optimizer, test_iter, False)
+
+    print('Training Data: accuracy: {} \t loss: {} \n'
+          'Testing Data: accuracy: {} \t loss: {}'.format(train_accuracy, train_loss,
+                                                          test_accuracy, test_loss))
+
+
+# MAIN PROGRAM START
+model, optimizer, results = get_model("convnet_v0")
+test_network(model, optimizer)
+
+plt.plot(results[1], label='Test Loss')
+plt.plot(results[0], label='Train Loss')
 plt.legend()
 plt.ylabel("Loss")
 plt.xlabel("Epoch")
