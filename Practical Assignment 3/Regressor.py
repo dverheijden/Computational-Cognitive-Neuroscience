b4697ddc5ebf0d8db71ccfcf1636ce5305f3d755
ScaleRunner <dennis-verheijden@live.nl>
2017-10-13 13:29:39 +0200

    PA3 something

--- Practical Assignment 3/Regressor.py
+++ Practical Assignment 3/Regressor.py
@@ -1,14 +1,19 @@
-from chainer import report
+from chainer import reporter
 from chainer import Chain
 import chainer.functions as F
+from chainer.functions.evaluation import accuracy
 
 
 class Regressor(Chain):
-	def __init__(self, RNN):
+	def __init__(self, predictor, lossfun=F.softmax_cross_entropy, accfun=accuracy.accuracy):
 		super(Regressor, self).__init__()
+		self.lossfun = lossfun
+		self.accfun = accfun
 		with self.init_scope():
-			self.model = RNN
+			self.predictor = predictor
 
 	def __call__(self, x, label):
-		y = self.model(x)
-		return y - label
+		self.y = self.model(x)
+		self.loss = self.lossfun(self.y, label)
+		self.accuracy = self.accfun(self.y, label)
+		reporter.report({'accuracy': self.accuracy}, self)
--- Practical Assignment 3/main.py
+++ Practical Assignment 3/main.py
@@ -1,15 +1,20 @@
 import numpy as np
-import chainer.links as L
-from chainer import optimizers
-from chainer import training
-from chainer.training import extensions
+import chainer
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
 from chainer.datasets import TupleDataset
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
 from Networks import RNN
 from Regressor import Regressor
+
+# create toy data - compute sum of the previous and current input
+from Regressor_v0 import Regressor_v0
 from utils import StreamingIterator
 
 
-# create toy data - compute sum of the previous and current input
 def create_data(n=3000):
 	X = np.random.rand(n, 1).astype('float32')
 	T = np.sum(np.hstack((X[0:-1], X[1:])), axis=1)
@@ -19,30 +24,62 @@ def create_data(n=3000):
 	return TupleDataset(X, T)
 
 
-def compute_loss(data):
+def compute_loss(x, label):
 	loss = 0
 
-	loss += model(data[0][0], data[1][0])
+	loss += model(x, label)
 
-	print(loss)
 	return loss
 
 
-def feed_data():
-	for data in StreamingIterator(dataset, 30):
+def train_network():
+	"""
+	Trains the network for n_epochs.
+	One epoch contains of a training phase and testing phase. Afterwards, the results are printed to the screen
+	"""
+
+	train_loss_list = []
+	test_loss_list = []
+
+	for epoch in range(n_epochs):
 		rnn.reset_state()
-		for i in data:
-			optimizer.update(compute_loss, i)
+		for data in train_iter:
+			train_loss, train_accuracy = feed_data(data)
+			test_loss, test_accuracy = feed_data(data)
+
+			train_loss_list.append(train_loss)
+			test_loss_list.append(test_loss)
+			print('Epoch {} \n'
+			      'Training: accuracy: {} \t loss: {} \n'
+			      'Testing: accuracy: {} \t loss: {}'.format(epoch + 1,
+			                                                 train_accuracy, train_loss,
+			                                                 test_accuracy, test_loss))
+
+	return [train_loss_list, test_loss_list]
 
 
-if __name__ == "__main__":
+def feed_data(data):
+
+	total_loss = 0
+	total_accuracy = 0
+	for i, label in data:
+		optimizer.update(compute_loss, i, label)
+		total_loss += float(model.loss.data)
+		total_accuracy += float(model.accuracy.data)
+	return total_loss / len(data), total_accuracy / len(data)
+
+def with_trainer():
 	epochs = 100
 
-	dataset = create_data()
+	train = create_data()
+	test = create_data()
+
+	train_iter = iterators.SerialIterator(train, batch_size=100, shuffle=False)
+	test_iter = iterators.SerialIterator(test, batch_size=100, repeat=False, shuffle=False)
 
 	rnn = RNN(n_hidden=50)
 
-	model = Regressor(rnn)
+	model = Regressor(rnn, compute_loss)
 
 	# Set up the optimizer
 	optimizer = optimizers.SGD()
@@ -60,3 +97,23 @@ if __name__ == "__main__":
 	trainer.extend(extensions.ProgressBar())
 
 	trainer.run()
+
+
+if __name__ == "__main__":
+	n_epochs = 5
+
+	train = create_data()
+	test = create_data()
+
+	train_iter = StreamingIterator(train, batch_size=3000)
+	test_iter = StreamingIterator(test, batch_size=200)
+
+	rnn = RNN(n_hidden=5)
+
+	model = Regressor_v0(rnn)
+
+	# Set up the optimizer
+	optimizer = optimizers.SGD()
+	optimizer.setup(model)
+
+	train_network()
